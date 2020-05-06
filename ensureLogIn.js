7cdc5a59dedbe392b08f009aad1a27001dadfefd
roshan <rdev.dev06@gmail.com>
2020-05-06 13:36:09 +0530

    addedPassport with jwt

--- ensureLogIn.js
+++ ensureLogIn.js
@@ -1,28 +1,32 @@
-const jwt = require('jsonwebtoken');
+const passport = require('passport');
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
 const config = require('./config');
+const opts = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: config.jwt_secrect_key
+};
+passport.use(new JwtStrategy(opts, (decode, done) => done(null, decode)));
+
 module.exports = (req, res, next) => {
-  function verifyUser(token) {
-    jwt.verify(token, config.jwt_secrect_key, (err, decode) => {
-      if (err) {
-        console.log(err);
-        res.json({ msg: 'Auth Failed!!' });
-      } else {
-        req.user = decode;
+  passport.authenticate(
+    'jwt',
+    { session: false },
+    (error, user, info, status) => {
+      if (error) {
+        console.log(error);
+      }
+      if (user) {
+        req.user = user;
         next();
       }
-    });
-  }
-  if (req.headers.authorization) {
-    try {
-      const getToken = req.headers.authorization.split(' ');
-      getToken[0] == 'Bearer'
-        ? verifyUser(getToken[1])
-        : res.json({ msg: 'Bearer token is not present in the authorization' });
-    } catch (error) {
-      console.log(error);
-      res.json({ msg: 'Auth Failed!!' });
+      if (info) {
+        res.send(info.message);
+      }
+      if (status) {
+        console.log('status');
+        console.log(status);
+      }
     }
-  } else {
-    res.json({ msg: 'Authorisation headeris not present' });
-  }
+  )(req, res, next);
 };
